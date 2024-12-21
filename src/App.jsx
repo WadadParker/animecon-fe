93ae195ec257460b5c7c807baebc195192a6824a
Wadad Parker <wadadparker5@gmail.com>
2024-12-21 13:19:54 +0530

    fix: routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,10 @@ function App() {
   return (
   <div>
     <Routes>
-      <Route path="/" element={LandingPage} />
-      <Route path="/home" element={Home} />
-      <Route path="/payment" element={Payment} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/payment" element={<Payment />} />
     </Routes>
-    <h1 className="font-ruddy text-3xl font-bold">Ruddy Bold</h1>
-    <p className="font-ruddy text-lg italic">Ruddy LightItalic</p>
-    <p className="font-ruddy text-base font-normal">Ruddy Regular</p>
-    <p className="font-ruddy text-lg font-black">Ruddy Black</p>
   </div>
   )
 }
