a718da4561a483e7aae2b566c63be53a8df0ef1f
Wadad Parker <wadadparker5@gmail.com>
2024-12-21 13:18:21 +0530

    feat: pages & routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Route , Routes } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import Home from "./pages/Home"
+import Payment from "./pages/Payment"
 
 function App() {
 
   return (
   <div>
+    <Routes>
+      <Route path="/" element={LandingPage} />
+      <Route path="/home" element={Home} />
+      <Route path="/payment" element={Payment} />
+    </Routes>
     <h1 className="font-ruddy text-3xl font-bold">Ruddy Bold</h1>
     <p className="font-ruddy text-lg italic">Ruddy LightItalic</p>
     <p className="font-ruddy text-base font-normal">Ruddy Regular</p>
