4073cb7708806d0b1d079ecc9bb5ace7d237004c
Shivank12batra <Shivank56batra@gmail.com>
2024-12-21 15:42:22 +0530

    fix: removed modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import LandingPage from "./pages/LandingPage";
-import Payment from "./pages/Payment";
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import LandingPage from "./pages/LandingPage"
+import { Payment } from "./pages/Payment"
 
 function App() {
   return (
@@ -9,10 +9,10 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/payment" element={<Payment setShowSuperModal={true} />} />
+        <Route path="/payment" element={<Payment amount={1200} />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,7 +1,7 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import { useEffect } from "react"
 
-const BuyButton = ({ amount, setShowSuperModal }) => {
+const Payment = ({ amount }) => {
+  console.log("LOG", amount)
   const checkoutHandler = () => {
     var option = {
       key: "rzp_test_gZUyFL8iSOmzRO",
@@ -10,13 +10,12 @@ const BuyButton = ({ amount, setShowSuperModal }) => {
       currency: "INR",
       name: "Client Name",
       description: "Checkout for Merch",
-      handler: function (response) {
-        setShowSuperModal(false); // Close modal on successful payment
+      handler: function () {
         const script = document.querySelector(
           'script[src="https://checkout.razorpay.com/v1/checkout.js"]'
-        );
+        )
         if (script) {
-          document.body.removeChild(script);
+          document.body.removeChild(script)
         }
       },
       prefill: {
@@ -30,25 +29,25 @@ const BuyButton = ({ amount, setShowSuperModal }) => {
       theme: {
         color: "#3399cc",
       },
-    };
+    }
 
-    var pay = new window.Razorpay(option);
-    pay.open();
-  };
+    var pay = new window.Razorpay(option)
+    pay.open()
+  }
   const razorpayHandler = () => {
     const loadRazorpay = async () => {
-      const script = document.createElement("script");
-      script.src = "https://checkout.razorpay.com/v1/checkout.js";
-      script.async = true;
+      const script = document.createElement("script")
+      script.src = "https://checkout.razorpay.com/v1/checkout.js"
+      script.async = true
       script.onload = () => {
         // Razorpay script loaded, now call the checkoutHandler
-        checkoutHandler();
-      };
-      document.body.appendChild(script);
-    };
+        checkoutHandler()
+      }
+      document.body.appendChild(script)
+    }
 
-    loadRazorpay();
-  };
+    loadRazorpay()
+  }
 
   useEffect(() => {
     // Clean up function to remove the script when the component unmounts
@@ -56,55 +55,23 @@ const BuyButton = ({ amount, setShowSuperModal }) => {
       // Remove the script if it exists
       const script = document.querySelector(
         'script[src="https://checkout.razorpay.com/v1/checkout.js"]'
-      );
+      )
       if (script) {
-        document.body.removeChild(script);
+        document.body.removeChild(script)
       }
-    };
-  }, [setShowSuperModal]);
-  return (
-    <button
-      className="bg-gradient-to-r from-[#407bff] to-[#f90707] text-white py-2 px-4 rounded-xl"
-      onClick={() => razorpayHandler()}
-    >
-      Pay!
-    </button>
-  );
-};
-const Payment = ({ setShowSuperModal }) => {
-  const [amount, setAmount] = useState(1);
+    }
+  }, [])
 
   return (
-    <div className="fixed w-screen h-screen top-0 left-0 z-40 bg-black/30 flex justify-center items-center">
-      <main className="p-4 rounded-xl border w-1/3 bg-white space-y-3">
-        <h1 className="text-2xl font-medium text-center">
-          Buy a <span className="gradient-text">SuperStar</span> for John!
-        </h1>
-        <aside className="flex space-x-1.5 items-center">
-          <b>Tip:</b>{" "}
-          <input
-            type={"number"}
-            onChange={(e) => setAmount(Number(e.target.value))}
-            className="bg-white border border-black w-16 appearance-none"
-            style={{ "-moz-appearance": "textfield" }}
-          />{" "}
-          <small>INR</small>
-        </aside>
-        <textarea
-          placeholder="Comment"
-          className=" p-4 outline-none border resize-none rounded-3xl bg-bg text-black text-2xl w-full"
-        />
-        <section className="flex justify-between">
-          <button
-            className="bg-black text-white rounded-md px-3 py-1"
-            onClick={() => setShowReviewModal(false)}
-          >
-            Cancel
-          </button>
-          <BuyButton amount={amount} setShowSuperModal={setShowSuperModal} />
-        </section>
-      </main>
+    <div className="flex items-center justify-center w-full h-full">
+      <button
+        className="bg-gradient-to-r from-[#407bff] to-[#f90707] text-white py-2 px-4 rounded-xl"
+        onClick={() => razorpayHandler()}
+      >
+        Pay!
+      </button>
     </div>
-  );
-};
-export default Payment;
+  )
+}
+
+export { Payment }
