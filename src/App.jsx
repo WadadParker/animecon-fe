f50668c944c872ac69fe2b5cb4bf31de6b2e752c
Shivank12batra <Shivank56batra@gmail.com>
2024-12-21 14:07:22 +0530

    feat: razorpay payment integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import { Route , Routes } from "react-router-dom"
-import LandingPage from "./pages/LandingPage"
-import Home from "./pages/Home"
-import Payment from "./pages/Payment"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
+import Payment from "./pages/Payment";
 
 function App() {
-
   return (
-  <div>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/payment" element={<Payment />} />
-    </Routes>
-  </div>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/payment" element={<Payment setShowSuperModal={true} />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
