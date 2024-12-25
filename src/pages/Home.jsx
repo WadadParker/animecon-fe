69118c91b69e21d15cb60242a3e8ae2c61c2c9ba
Wadad Parker <wadadparker5@gmail.com>
2024-12-25 23:11:28 +0530

    feat: Shadow Text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='font-ruddy bg-bg-light w-full'>Home</div>
+    <div className='font-ruddy bg-bg-light w-full'>
+      <section className='relative'>
+        <aside className=''>
+          <p className='absolute text-9xl bg-transparent font-bold tracking-[-0.020em] text-primary-red z-10' style={{WebkitTextStroke:"2.34px #9C0000"}}>Veena San</p>
+        </aside>
+        <p className='absolute top-2 left-2.5 text-9xl font-bold tracking-[-0.020em] text-black ' style={{WebkitTextStroke:"2.34px black"}}>Veena San</p>
+      </section>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,16 @@ export default {
       colors:{
         primary:{
           DEFAULT:"#F2C300",
+          red:"#D70000",
+          blue:"#3590FF",
           dark:"#FFCE00",
-          blue:"#3590FF"
         },
         secondary:"#2E63A4",
         bg:{
           light:"#FFFCEC"
+        },
+        stroke:{
+          DEFAULT:"#9C0000"
         }
       },
       fontFamily: {
