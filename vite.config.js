9084d30b25a141d6552bb9e3237b25bd7eb2b520
Wadad Parker <wadadparker5@gmail.com>
2025-02-23 15:12:36 +0530

    feat: root route

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { reactRouter } from "@react-router/dev/vite"
 const ReactCompilerConfig = { /* ... */ };
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
+  plugins: [reactRouter({
     babel: {
       plugins: [
         ["babel-plugin-react-compiler",ReactCompilerConfig]
